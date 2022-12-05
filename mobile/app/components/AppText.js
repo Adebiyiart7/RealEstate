0cc97d11350bf1569642071c0da6e9928c5780d4
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-12-05 05:24:58 +0100

    Added numberOfLines property

--- mobile/app/components/AppText.js
+++ mobile/app/components/AppText.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, } from 'react-native'
 // LOCAL IMPORTS
 import defaultStyles from '../config/styles'
 
-const AppText = ({ style, children }) => {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+const AppText = ({ style, children, ...otherProps }) => {
+  return <Text {...otherProps} style={[styles.text, style]}>{children}</Text>;
 };
 
 export default AppText;

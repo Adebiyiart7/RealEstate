2d46722b7269d355459f61b1724aacec416867b6
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-12-05 14:50:04 +0100

    initialized the SetupSuccess Screen

---
+++ mobile/app/screens/SetupSuccessScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SetupSuccessScreen = () => {
+  return (
+    <View>
+      <Text>SetupSuccessScreen</Text>
+    </View>
+  )
+}
+
+export default SetupSuccessScreen
+
+const styles = StyleSheet.create({})

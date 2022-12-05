0f1ae774f7fb6765a0e20b65d7e8df48257b5d55
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-12-05 05:26:09 +0100

    Updated

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,10 @@
 import LetsYouIn from './app/screens/LetsYouInScreen';
 import LoginScreen from './app/screens/LoginScreen';
+import ProfileScreen from './app/screens/ProfileScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
 
 export default function App() {
   return (
-    <LoginScreen />
+    <ProfileScreen />
   );
 }

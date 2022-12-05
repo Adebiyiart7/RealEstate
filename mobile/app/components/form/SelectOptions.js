3dc7f6d726b40c9838b86c20f72de7e6ede736fd
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-12-05 11:22:22 +0100

    Initialized the create new pin screen

---
+++ mobile/app/components/form/SelectOptions.js
@@ -0,0 +1,44 @@
+import { FlatList, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+
+// LOCAL IMPORTS
+import AppText from "../AppText";
+import ItemSeparatorComponent from "../ItemSeparatorComponent";
+
+const SelectOptions = ({
+  data,
+  fieldName,
+  setFieldValue,
+  setBottomSheetVisible
+}) => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.value.toString()}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => {
+            setFieldValue(fieldName, item.value);
+            setBottomSheetVisible(false);
+          }}
+          style={styles.item}
+        >
+          <AppText style={styles.text}>{item.value}</AppText>
+        </TouchableOpacity>
+      )}
+      ItemSeparatorComponent={() => <ItemSeparatorComponent />}
+    />
+  );
+};
+
+export default SelectOptions;
+
+const styles = StyleSheet.create({
+  item: {
+    paddingVertical: 12,
+    paddingHorizontal: 16
+  },
+  text: {
+    fontSize: 16
+  }
+});
---
+++ mobile/app/screens/CreateNewPin.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CreateNewPin = () => {
+  return (
+    <View>
+      <Text>CreateNewPin</Text>
+    </View>
+  )
+}
+
+export default CreateNewPin
+
+const styles = StyleSheet.create({})

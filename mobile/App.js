c9eb831894f549d30c2a827018d00cabf8f7bfc1
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-12-06 01:10:40 +0100

    Update

--- mobile/App.js
+++ mobile/App.js
@@ -3,8 +3,7 @@ import LetsYouIn from './app/screens/LetsYouInScreen';
 import LoginScreen from './app/screens/LoginScreen';
 import ProfileScreen from './app/screens/ProfileScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
-import SetupSuccessScreen from './app/screens/SetupSuccess';
 
 export default function App() {
-  return <SetupSuccessScreen />;
+  return <CreateNewPin />;
 }

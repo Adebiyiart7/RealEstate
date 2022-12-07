7daa8a24b4d0c63f798a00da01c720eff129a2ff
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-12-07 11:02:53 +0100

    Updated

--- api/index.js
+++ api/index.js
@@ -2,8 +2,18 @@ require("dotenv").config();
 const port = process.env.PORT;
 const express = require("express");
 
+// LOCAL IMPORTS
+const errorMiddleware = require("./middleware/error");
+const connectDB = require("./config/db");
+
+connectDB();
 const app = express();
 
-app.use("/api/users", require("./routes/user"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", require("./routes/user"));
+
+app.use(errorMiddleware);
 
 app.listen(port, () => console.log("Server running on port", port));

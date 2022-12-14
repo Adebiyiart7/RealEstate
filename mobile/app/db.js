017c95ba180f0437237e26070ab364e5174a5be0
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-12-14 20:26:14 +0100

    Created fake db

---
+++ mobile/app/db.js
@@ -0,0 +1,149 @@
+export const featured = [
+  {
+    _id: "2",
+    image: require("./assets/images/building2.jpg"),
+    name: "Zenith Apartments",
+    location: "Lagos",
+    cost: 64000,
+    duration: "night",
+    rating: 4.9
+  },
+  {
+    _id: "1",
+    image: require("./assets/images/building1.jpg"),
+    name: "Kings and Queens Apartments",
+    location: "Abuja",
+    cost: 480000,
+    duration: "night",
+    rating: 4.7
+  },
+  {
+    _id: "3",
+    image: require("./assets/images/building3.jpg"),
+    name: "Gwarimpa Estate",
+    location: "Abuja",
+    cost: 11000000,
+    duration: "year",
+    rating: 5.0
+  },
+  {
+    _id: "4",
+    image: require("./assets/images/building4.jpg"),
+    name: "Adewale Hotels",
+    location: "Akure",
+    cost: 35000,
+    duration: "night",
+    rating: 4.7
+  }
+];
+
+export const ourRecommendation = [
+  {
+    _id: "1",
+    image: require("./assets/images/interior1.jpg"),
+    name: "Adewale Hotels",
+    location: "Akure",
+    cost: 35000,
+    duration: "night",
+    rating: 4.7
+  },
+  {
+    _id: "2",
+    image: require("./assets/images/interior2.jpg"),
+    name: "Biyi Homes",
+    location: "Abuja",
+    cost: 90000,
+    duration: "night",
+    rating: 4.7
+  },
+  {
+    _id: "3",
+    image: require("./assets/images/interior3.jpg"),
+    name: "Kings Hotels",
+    location: "Ikeja",
+    cost: 46000,
+    duration: "night",
+    rating: 4.8
+  },
+  {
+    _id: "4",
+    image: require("./assets/images/interior4.jpg"),
+    name: "Zenith Apartment",
+    location: "Enugu",
+    cost: 28000,
+    duration: "night",
+    rating: 4.7
+  },
+  {
+    _id: "5",
+    image: require("./assets/images/interior5.jpg"),
+    name: "Smith Hotels",
+    location: "Nasarawa",
+    cost: 44000,
+    duration: "night",
+    rating: 4.3
+  },
+  {
+    _id: "6",
+    image: require("./assets/images/interior6.jpg"),
+    name: "International Hotels",
+    location: "Kogi",
+    cost: 55000,
+    duration: "night",
+    rating: 4.5
+  },
+  {
+    _id: "7",
+    image: require("./assets/images/interior12.jpg"),
+    name: "Travellers Hotels",
+    location: "Minna",
+    cost: 35000,
+    duration: "night",
+    rating: 4.8
+  },
+  {
+    _id: "8",
+    image: require("./assets/images/interior8.jpg"),
+    name: "Portable Homes",
+    location: "Abuja",
+    cost: 35000,
+    duration: "night",
+    rating: 4.1
+  },
+  {
+    _id: "9",
+    image: require("./assets/images/interior9.jpg"),
+    name: "Unique Homes",
+    location: "Edo",
+    cost: 59000,
+    duration: "night",
+    rating: 4.4
+  },
+  {
+    _id: "10",
+    image: require("./assets/images/interior10.jpg"),
+    name: "Senetors Hotels",
+    location: "Benin",
+    cost: 95000,
+    duration: "night",
+    rating: 4.9
+  },
+  {
+    _id: "11",
+    image: require("./assets/images/interior11.jpg"),
+    name: "Banana Hotels",
+    location: "Ogun",
+    cost: 26000,
+    duration: "night",
+    rating: 4.2
+  },
+  {
+    _id: "12",
+    image: require("./assets/images/interior12.jpg"),
+    name: "Classic Homes",
+    location: "Osun",
+    cost: 69000,
+    duration: "night",
+    rating: 4.6
+  }
+];

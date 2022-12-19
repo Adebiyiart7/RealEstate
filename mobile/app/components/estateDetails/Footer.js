c1c4a076620594058990f1deff64725fbdc39a21
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-12-19 21:42:34 +0100

    Created footer component

---
+++ mobile/app/components/estateDetails/Footer.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import AppButton from "../AppButton";
+import AppText from "../AppText";
+import colors from "../../config/colors";
+import { NavigationContainer } from "@react-navigation/native";
+import routes from "../../config/routes";
+
+const Footer = ({ navigation, item }) => {
+  return (
+    <View style={styles.footer}>
+      <View style={styles.left}>
+        <AppText style={styles.priceText}>Price</AppText>
+        <AppText style={styles.price}>
+          &#8358;{parseInt(item.cost).toLocaleString()}
+          <AppText style={styles.duration}> / {item.duration}</AppText>
+        </AppText>
+      </View>
+      <AppButton style={styles.button} onPress={() => NavigationContainer.navigate(routes.BOOKING)}>Booking Now</AppButton>
+    </View>
+  );
+};
+
+export default Footer;
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1
+  },
+  duration: {
+    fontWeight: 400,
+    fontSize: 13,
+    color: colors.mediumText
+  },
+  footer: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 20,
+    borderTopWidth: 1,
+    borderColor: colors.border100,
+    borderRadius: 25,
+    padding: 16,
+  },
+  price: {
+    color: colors.primaryColor,
+    fontSize: 25,
+    fontWeight: "bold",
+    marginRight: 16
+  },
+  priceText: {
+    fontSize: 13,
+    color: colors.lightText
+  }
+});

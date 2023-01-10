4097564bea5af0ea819c15ef5cfd0281505d8ca5
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-01-10 17:06:57 +0100

    Added get me controller

--- api/controllers/user/me.js
+++ api/controllers/user/me.js
@@ -1,18 +1,21 @@
 const User = require("../../models/user");
 const { apiResponse } = require("../../utils");
+
 /**
  * @route         GET   /api/users/me
  * @description   get all user's details
+ * @access        private
  */
-
 const me = async (req, res) => {
-  const user = await User.findOne(req.body);
+  const user = await User.findOne(req.body.email).select("-password", "-verificationCode");
 
   if (!user) {
-    
+    res.status(404);
+    throw new Error("User not found!");
   }
 
-  res.status(200).json(apiResponse(res.statusCode, "", user)) // TODO: Remove password 
+  console.log(user);
+  res.status(200).json(apiResponse(res.statusCode, "", user))
 }
 
 module.exports = me;

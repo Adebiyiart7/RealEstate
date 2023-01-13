8968fffdd9414f6371abf0152f7defbc410a26d1
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-01-13 17:33:36 +0100

    Updated messages screen

--- mobile/app/screens/MessagesScreen.js
+++ mobile/app/screens/MessagesScreen.js
@@ -1,14 +1,80 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import Screen from "../components/Screen";
+import GoBackArrowHeader from "../components/GoBackArrowHeader";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+import AppButton from "../components/AppButton";
+import Chats from "../components/messages/Chats";
+import Calls from "../components/messages/Calls";
 
-const MessagesScreen = () => {
+const MessagesScreen = ({ navigation }) => {
+  const [showChats, setShowChats] = useState(true);
+
+  const activeTabStyle = {
+    borderBottomColor: colors.primaryColor,
+    borderBottomWidth: 3,
+  }
+
+  const activeTabTextStyle = {
+    color: colors.primaryColor
+  };
+  
   return (
-    <View>
-      <Text>Messages</Text>
-    </View>
-  )
-}
+    <Screen scrollable={false}>
+      <GoBackArrowHeader
+        isTabScreen
+        navigation={navigation}
+        title={"Messages"}
+        RightIcon={
+          <Ionicons
+            size={24}
+            name={"ios-search-outline"}
+            style={{ colors: colors.primaryText }}
+          />
+        }
+        RightIconExtra={
+          <MaterialCommunityIcons
+            name="dots-horizontal-circle-outline"
+            size={24}
+            style={{ colors: colors.primaryText }}
+          />
+        }
+      />
+      <View style={styles.tabs}>
+        <AppButton
+        onPress={() => setShowChats(true)}
+        style={[styles.tab, showChats ? activeTabStyle : {}]} textStyle={[styles.tabText,showChats ? activeTabTextStyle : {}]} secondary>
+          Chats
+        </AppButton>
+        <AppButton
+        onPress={() => setShowChats(false)}
+        style={[styles.tab, !showChats ? activeTabStyle : {}]} textStyle={[styles.tabText, !showChats ? activeTabTextStyle : {}]} secondary>
+          Calls
+        </AppButton>
+      </View>
+      <View>{showChats ? <Chats /> : <Calls />}</View>
+    </Screen>
+  );
+};
 
-export default MessagesScreen
+export default MessagesScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  tab: {
+    flex: 1,
+    backgroundColor: "transparent",
+    paddingVertical: 5,
+    borderRadius: 0,
+  },
+  tabs: {
+    display: "flex",
+    flexDirection: "row"
+    // borderBottomColor: colors.border200,
+    // borderBottomWidth: 1
+  },
+  tabText: {
+    fontSize: 18,
+    color: colors.lightText
+  }
+});

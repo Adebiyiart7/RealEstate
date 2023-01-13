8d326f2ee199a042c1a2af988ec3ade5151df0aa
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-01-13 17:37:11 +0100

    Complete remove favorite bottom sheet

--- mobile/app/components/RemoveFavoriteContent.js
+++ mobile/app/components/RemoveFavoriteContent.js
@@ -4,25 +4,36 @@ import { Header } from "./PropertiesFilterContent";
 import AppButton from "./AppButton";
 import Card3 from "./cards/Card3";
 import { estates } from "../db";
+import colors from "../config/colors";
 
-const RemoveFavoriteContent = ({ setBottomSheetVisibleRemoveFav }) => {
+const RemoveFavoriteContent = ({
+  setBottomSheetVisibleRemoveFav,
+  navigation
+}) => {
   return (
     <View>
-      <Header title={"Remove from Favorites?"} />
-      <View style={styles.item}>
-        <Card3 navigation={navigation} format={"list"} item={estates[6]} />
-      </View>
-      <View style={styles.buttons}>
-        <AppButton
-          small
-          style={styles.button}
-          onPress={() => setBottomSheetVisibleRemoveFav(false)}
-        >
-          Cancel
-        </AppButton>
-        <AppButton small style={styles.button}>
-          Yes, Remove
-        </AppButton>
+      <Header
+        style={{ fontSize: 20, textAlign: "center" }}
+        title={"Remove from Favorites?"}
+      />
+      <View style={styles.content}>
+        <View style={styles.item}>
+          <Card3 navigation={navigation} format={"list"} item={estates[6]} />
+        </View>
+        <View style={styles.buttons}>
+          <AppButton
+           
+            rounded
+            secondary
+            style={{ ...styles.button, marginRight: 8 }}
+            onPress={() => setBottomSheetVisibleRemoveFav(false)}
+          >
+            Cancel
+          </AppButton>
+          <AppButton  rounded style={{ ...styles.button, marginLeft: 8 }}>
+            Yes, Remove
+          </AppButton>
+        </View>
       </View>
     </View>
   );
@@ -38,5 +49,6 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between"
-  }
+  },
+  content: { padding: 16, borderTopColor: colors.border100, borderTopWidth: 1, }
 });

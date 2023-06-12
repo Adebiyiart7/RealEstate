79f5f13a86214aa3d8379490796131967f47796f
Adeeyo Joseph Adebiyi <66339878+Adebiyiart7@users.noreply.github.com>
2023-06-12 17:08:35 +0100

    Delete app2 directory

--- app2/features/config.js
+++
@@ -1,8 +0,0 @@
-export const API_URI = "https://realestate-api.onrender.com/api";
-export const axiosConfig = (token) => {
-  return {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-};

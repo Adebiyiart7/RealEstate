c149015c9e530ac819c8349ec1b41a2eba4b9ea1
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-08-09 09:04:08 +0100

    Added AsyncStorageName for saving favorite homes ID

--- mobile/app/config/constants.js
+++ mobile/app/config/constants.js
@@ -1,7 +1,11 @@
 const constants = {
   passwordRegEx: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/,
   passwordError:
-    "Password must be more than 8 chars, have at least one number, at least one special character(!@#$%^&*), at least one uppercase and one lowercase."
+    "Password must be more than 8 chars, have at least one number, at least one special character(!@#$%^&*), at least one uppercase and one lowercase.",
+
+  asyncStorageNames: {
+    favoriteHomes: "@favorites",
+  },
 };
 
 export default constants;

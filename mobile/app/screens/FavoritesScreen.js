37252fb92b0a508082e42662cd34811516d5da6a
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-09-10 08:57:31 +0100

    Completed the favorite screen

--- mobile/app/screens/FavoritesScreen.js
+++ mobile/app/screens/FavoritesScreen.js
@@ -1,17 +1,32 @@
-import { StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import React, { useState } from "react";
-import OurRecommendation from "../components/OurRecommendation";
 import GoBackArrowHeader from "../components/GoBackArrowHeader";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 import BottomSheet from "../components/BottomSheet";
 import PropertiesFilterContent from "../components/PropertiesFilterContent";
-import RemoveFavoriteContent from "../components/RemoveFavoriteContent";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+// LOCAL IMPORTS
+import Chip from "../components/Chip";
+import Card3 from "../components/cards/Card3";
+import { estateCategory, estates } from "../db";
+import AppText from "../components/AppText";
+import defaultStyles from "../config/styles";
+import PropertyDisplayFormat from "../components/PropertyDisplayFormat";
+import { useFavorite } from "../contexts/FavoriteHomeContext";
+import utils from "../utils";
 
 const FavoritesScreen = ({ navigation }) => {
+  const { state } = useFavorite();
+  const [focusedItem, setFocusedItem] = useState("All");
+  const [displayFormat, setDisplayFormat] = useState("grid"); // list or grid
   const [bottomSheetVisible, setBottomSheetVisible] = useState(false);
-  const [bottomSheetVisibleRemoveFav, setBottomSheetVisibleRemoveFav] = useState(true);
+
+  const favoriteHomes = estates.filter((estate) =>
+    state.favorites.includes(estate._id)
+  );
 
   return (
     <Screen style={styles.container}>
@@ -20,16 +35,7 @@ const FavoritesScreen = ({ navigation }) => {
         setBottomSheetVisible={setBottomSheetVisible}
         bottomSheetContent={<PropertiesFilterContent />}
       />
-      <BottomSheet
-        bottomSheetContent={
-          <RemoveFavoriteContent
-            navigation={navigation}
-            setBottomSheetVisibleRemoveFav={setBottomSheetVisibleRemoveFav}
-          />
-        }
-        bottomSheetVisible={bottomSheetVisibleRemoveFav}
-        setBottomSheetVisible={setBottomSheetVisibleRemoveFav}
-      />
+
       <GoBackArrowHeader
         isTabScreen
         navigation={navigation}
@@ -50,15 +56,81 @@ const FavoritesScreen = ({ navigation }) => {
           />
         }
       />
-      <OurRecommendation
-        navigation={navigation}
-        categoryChipStyle={{ marginTop: 0 }}
-        propertyCount={"2484 favorites"}
-      />
+      <View style={styles.container}>
+        <FlatList
+          horizontal
+          style={{ marginTop: 0 }}
+          showsHorizontalScrollIndicator={false}
+          data={estateCategory}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => (
+            <View>
+              <Chip
+                text={item.name}
+                focused={item.name === focusedItem}
+                Icon={
+                  item.icon && (
+                    <MaterialCommunityIcons
+                      name={item.icon}
+                      size={18}
+                      color={
+                        item.name === focusedItem
+                          ? colors.white
+                          : colors.primaryColor
+                      }
+                    />
+                  )
+                }
+                onPress={() => setFocusedItem(item.name)}
+              />
+            </View>
+          )}
+          ItemSeparatorComponent={() => <AppText style={styles.seperator} />}
+        />
+        <PropertyDisplayFormat
+          displayFormat={displayFormat}
+          propertyCount={`${state.favorites.length} ${utils.pluralize(
+            state.favorites.length,
+            "favorite"
+          )}`}
+          setDisplayFormat={setDisplayFormat}
+        />
+        <View style={defaultStyles.gridStyle}>
+          {favoriteHomes.map((item) => (
+            <View key={item._id}>
+              <Card3
+                navigation={navigation}
+                format={displayFormat}
+                item={item}
+                state={state}
+              />
+            </View>
+          ))}
+        </View>
+      </View>
     </Screen>
   );
 };
 
 export default FavoritesScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 50,
+  },
+  featuredView: {
+    marginTop: 10,
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  ourRecommendation: {
+    marginBottom: 16,
+    marginTop: 20,
+  },
+  seperator: {
+    marginRight: 10,
+  },
+});

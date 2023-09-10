b1536d146a311f37477330bf0ab8cb7ee13297ee
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-09-10 08:58:43 +0100

    Completed the ComfirmPinScreeb

--- mobile/app/screens/ConfirmPinScreen.js
+++ mobile/app/screens/ConfirmPinScreen.js
@@ -14,6 +14,8 @@ import { useSelector } from "react-redux";
 const ConfirmPinScreen = ({ navigation, route }) => {
   const { user } = useSelector((state) => state.auth);
   const [bottomSheetVisible, setBottomSheetVisible] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showFailure, setShowFailure] = useState(false);
   const item = estates.find((i) => i._id === route.params._id);
   const userInfo = route.params.userInfo;
   const [pin, setPin] = useState({
@@ -23,65 +25,94 @@ const ConfirmPinScreen = ({ navigation, route }) => {
     fourth: "",
   });
 
+  // TODO: Fake confirmation
+  const handleCorrectPin = () => {
+    if (pin.length === 4) {
+      setShowSuccess(true);
+    } else {
+      setShowFailure(true);
+    }
+  };
+
   return (
     <>
       <LoginBottomSheet
         bottomSheetVisible={bottomSheetVisible}
         setBottomSheetVisible={setBottomSheetVisible}
       />
-      <ProcessStatus
-        status="success"
-        title={"Congratulations!"}
-        image={require("../assets/images/success-image.png")}
-        message={`${item.name} successfully booked, you can check your bookings on the menu Profile -> My Booking`}
-        Actions={
-          <View style={{ marginTop: 10 }}>
-            <AppButton
-              small
-              onPress={() => {
-                user
-                  ? navigation.navigate(routes.E_RECEIPT, {
-                      _id: route.params._id,
-                      checksDetails: route.params.checksDetails,
-                      userInfo: userInfo,
-                    })
-                  : setBottomSheetVisible(true);
-              }}
-              rounded
-              style={{ width: "100%", marginBottom: 0 }}
-            >
-              View E-Receipt
-            </AppButton>
-            <AppButton small rounded secondary>
-              My Booking
-            </AppButton>
-          </View>
-        }
-      />
-      {/* <ProcessStatus
-        status="failure"
-        title={"Ops, Failed!"}
-        image={require("../assets/images/failure-image.png")}
-        message={
-          "Your payment failed, please check your internet and try again!"
-        }
-        Actions={
-          <View style={{ marginTop: 10 }}>
-            <AppButton rounded style={{ width: "100%", marginBottom: 0 }}>
-              Try Again
-            </AppButton>
-            <AppButton rounded secondary>
-              Cancel
-            </AppButton>
-          </View>
-        }
-      /> */}
+
+      {showSuccess && (
+        <ProcessStatus
+          status="success"
+          title={"Congratulations!"}
+          image={require("../assets/images/success-image.png")}
+          message={`${item.name} successfully booked, you can check your bookings on the menu Profile -> My Booking`}
+          Actions={
+            <View style={{ marginTop: 10 }}>
+              <AppButton
+                onPress={() => {
+                  user
+                    ? navigation.navigate(routes.E_RECEIPT, {
+                        _id: route.params._id,
+                        checksDetails: route.params.checksDetails,
+                        userInfo: userInfo,
+                      })
+                    : setBottomSheetVisible(true);
+                }}
+                rounded
+                style={{ width: "100%", marginBottom: 0 }}
+              >
+                View E-Receipt
+              </AppButton>
+              {/* <AppButton
+                small
+                rounded
+                secondary
+                onPress={() => setShowSuccess(false)}
+              >
+                Cancel
+              </AppButton> */}
+            </View>
+          }
+        />
+      )}
+
+      {showFailure && (
+        <ProcessStatus
+          status="failure"
+          title={"Ops, Failed!"}
+          image={require("../assets/images/failure-image.png")}
+          message={
+            "Your payment failed, please check your internet and try again!"
+          }
+          Actions={
+            <View style={{ marginTop: 10 }}>
+              <AppButton
+                rounded
+                onPress={() => setShowFailure(false)}
+                style={{ width: "100%", marginBottom: 0 }}
+              >
+                Try Again
+              </AppButton>
+              <AppButton
+                rounded
+                secondary
+                onPress={() => setShowFailure(false)}
+              >
+                Cancel
+              </AppButton>
+            </View>
+          }
+        />
+      )}
       <Screen>
         <GoBackArrowHeader navigation={navigation} title={"Enter Your Pin"} />
         <View style={styles.container}>
           <AppText style={styles.text}>Enter your PIN</AppText>
           <PinForm pin={pin} setPin={setPin} secureTextEntry />
-          <AppButton style={styles.button}>Continue</AppButton>
+          <AppButton style={styles.button} onPress={() => handleCorrectPin()}>
+            Continue
+          </AppButton>
         </View>
       </Screen>
     </>

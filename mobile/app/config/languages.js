0611f6c45461ce18bfca62ff36349cbdda644fed
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-09-16 16:33:44 +0100

    Created a temporary list of language

---
+++ mobile/app/config/languages.js
@@ -0,0 +1,13 @@
+const languages = [
+  { name: "English (US)", suggested: true, available: true },
+  { name: "English (UK)", suggested: true, available: true },
+  { name: "Mandarin", available: true },
+  { name: "Spanish", available: true },
+  { name: "French", available: true },
+  { name: "Arabic", available: true },
+  { name: "Bengali", available: false },
+  { name: "Russian", available: true },
+  { name: "Indonasia", available: false },
+];
+
+export default languages;
